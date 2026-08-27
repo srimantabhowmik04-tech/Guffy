ae109e47e45d6acc88b3fdfc0c26e47d96b41831
srimantabhowmik04-tech <srimantabhowmik04@gmail.com>
2026-08-27 12:07:42 +0530

    Update login_screen.dart

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -118,3 +118,23 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 }
+  void _login() async {
+    setState(() => _isLoading = true);
+    String res = await AuthService().loginUser(
+      email: _emailController.text,
+      password: _passwordController.text,
+    );
+    setState(() => _isLoading = false);
+
+    if (res == "success" && mounted) {
+      // সফলভাবে লগইন হলে MainScreen-এ পাঠিয়ে দেওয়া
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const MainScreen()),
+      );
+    } else if (mounted) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(res)),
+      );
+    }
+  }
